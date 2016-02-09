a929b49ea68db6414ff37d75741c05f0b8b1c470
Rahul <rahulna@thoughtworks.com>
2016-02-09 18:57:26 +0530

    [Shruti/Rahul] - Used line instead of rectangle
    What changes have you made?
    
    *
    
    Why do we need this change?
    
    *
    
    What can this change impact?
    
    *

--- public/js/render.js
+++ public/js/render.js
@@ -6,30 +6,40 @@ d3.json('/data.json',function(err,json){
     var data = json;
     var holder = d3.select("body")
                     .append("svg")
-                    .attr("width", 1000)
-                    .attr('height', 750);
+                    .attr("width", 1500)
+                    .attr('height', 850)
     visualizeData(holder, data);
+    holder.append("line")
+        .attr({x1: 0, x2: 1500, y1: 750, y2: 750})
+        .style({stroke: "black", "stroke-width": 1});
+    holder.append("line")
+        .attr({x1: 0, x2: 0, y1: 0, y2: 750})
+        .style({stroke: "black", "stroke-width": 1});
 });
 
 var visualizeData = function(holder, data){
-    var rectGroup = holder
+    var lineGroup = holder
                         .selectAll("g")
                         .data(data)
                         .enter()
                         .append("g")
 
-    var rect = rectGroup
-                    .append("rect")
-                    .attr("x", 10)
-                    .attr("y", function(d, index){return index*25;})
-                    .attr("width", function(d){return scale(d.count)})
-                    .attr("height", 20)
-                    .style("fill", function(d, index){return index%2 == 0 ? "steelblue" : "skyblue"})
-                    .style("stroke",function(d, index){return index%2 == 0 ? "skyblue" : "steelblue"})
+    var bar = lineGroup
+                    .append("line")
+                    .attr("x1", function(d, index){return (index*50)+20;})
+                    .attr("x2", function(d, index){return (index*50)+20;})
+                    .attr("y1", 750)
+                    .attr("y2", function(d){return 700 - scale(d.count)})
+                    .style("stroke","steelblue")
+                    .style("stroke-width",20);
 
-    var text = rectGroup
-                    .append("text");
+    lineGroup.append("title")
+            .text(function(d){return d.count;});
+    var textGroup = lineGroup.append('g');
+                textGroup
+                    .append("text")
                     .text(function(d){return d.name;})
-                    .attr("x",function(d){return scale(d.count)+20})
-                    .attr("y",function(d,index){return (25*index)+15;});
+                    .attr("x",function(d, i){return (i*50)+10;})
+                    .attr("y", 780)
+    // textGroup.attr("transform", "rotate(45 0 0)");
 }
