fc28925036830c9e3eb4ebb78a1c42c9e2efc8ab
Rahul <rahulna@thoughtworks.com>
2016-02-11 16:15:24 +0530

    counted isSwipe, isSugarless, total consumption  and visualized only the total consumption

--- public/js/render.js
+++ public/js/render.js
@@ -1,13 +1,30 @@
+var margin = {
+    top: 30,
+    right: 40,
+    left: 40,
+    bottom: 30
+};
+var width = 1200 - margin.left - margin.right;
+var height = 600 - margin.top - margin.bottom;
+//
+// var x = d3.scale.ordinal()
+//     .rangeRoundBands([0, width], 0.1);
+// var y = d3.scale.linear()
+//     .range([height, 0]);
+//
+// var yAxis = d3.svg.axis()
+//     .scale(y)
+//     .orient("left")
 var scale = d3.scale.linear()
                 .domain([0,6000])
                 .range([0,700]);
 
 d3.json('/data.json',function(err,json){
     var data = json;
-    var holder = d3.select("body")
+    var holder = d3.select(".canvas")
                     .append("svg")
-                    .attr("width", 1500)
-                    .attr('height', 850)
+                    .attr("width", width + margin.left + margin.right)
+                    .attr('height', width + margin.top + margin.bottom)
     visualizeData(holder, data);
     holder.append("line")
         .attr({x1: 0, x2: 1500, y1: 750, y2: 750})
@@ -23,18 +40,19 @@ var visualizeData = function(holder, data){
                         .data(data)
                         .enter()
                         .append("g");
+                        // .call(yAxis);
 
     var bar = lineGroup
                     .append("line")
                     .attr("x1", function(d, index){return (index*50)+20;})
                     .attr("x2", function(d, index){return (index*50)+20;})
                     .attr("y1", 750)
-                    .attr("y2", function(d){return 700 - scale(d.count)})
+                    .attr("y2", function(d){return 700 - scale(d.total_count)})
                     .style("stroke","steelblue")
                     .style("stroke-width",20);
 
     lineGroup.append("title")
-            .text(function(d){return d.count;});
+            .text(function(d){return d.total_count;});
 
     var textGroup = lineGroup.append('g');
 
